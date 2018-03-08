a1051ec806995f06e89c7b550e34ad0cca037675
jmjuanes <josemijuanes@gmail.com>
2018-03-09 00:20:41 +0100

    Replaced log manager with logger imported from lib. Added method to get the options passed to the script. Export log and error.

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 let keue = require("keue");
 let getArgs = require("get-args");
-let logty = require("logty");
 
 //Log manager
-let logger = new logty(null, {encoding: "utf8"});
-logger.setFormat(function(tag, label, message){
-    return "[" + logty.timestamp("YYYY-MM-DD hh:mm:ss") + "] " + message;
-});
-logger.pipe(process.stdout);
+let logger = require("./lib/logger.js");
 
-//Tasks finished method
-let tasksFinished = function() {
-    logger.end();
-};
+//Parse the options
+let options = getArgs().options;
 
 //Tasks manager
 let tasks = new keue();
 let tasksList = []; //List of added tasks
 let taskStart = 0, taskEnd = 0; //Task timers
 tasks.on("finish", function () {
-    return tasksFinished();
+    logger.end();
 });
 tasks.on("task:start", function (name) {
     taskStart = Date.now();
-    return logger.debug("Starting task " + name + "");
+    return logger.log("Task '" + logger.colors.blue(name) + "' started.");
 });
 tasks.on("task:end", function(name){
     taskEnd = Date.now();
-    return logger.debug("Task " + name + " finished after " + (taskEnd - taskStart) + "ms");
+    let time = taskEnd - taskStart;
+    return logger.log("Task '" + logger.colors.blue(name) + "' finished after " + time + "ms.");
 });
 tasks.on("error", function(error){
-    logger.debug(error.message);
-    return tasksFinished();
+    logger.error(error.message);
+    logger.end();
 });
 
 //Add a new task
@@ -41,21 +35,31 @@ module.exports.task = function () {
 };
 
 //Print a log message in console
-module.exports.log = function(message) {
-    logger.customLabeledMessage("", message);
+module.exports.log = logger.log;
+module.exports.error = logger.error;
+
+//Access to the options passed to the script
+module.exports.option = function(key) {
+    return options[key];
 };
 
-//Next tick
 process.nextTick(function () {
-    let options = getArgs().options;
-    //console.log(options);
-    let list = (typeof options.tasks === "string") ? options.tasks.split(",") : tasksList;
-    if(list.length === 0) {
-        logger.customLabeledMessage("", "No tasks to run");
-        return tasksFinished();
+    //Check the no-colors option
+    if(typeof options["no-colors"] === "boolean") {
+        logger.disableColors();
     }
-    //Run the provided tasks
+    //Get the tasks to run
+    let list = (typeof options["tasks"] === "string") ? options["tasks"].split(",") : tasksList;
+    if(list.length === 0) {
+        logger.error("No tasks to run");
+        return logger.end();
+    }
     process.nextTick(function(){
-        return tasks.run(list);
+        try {
+            tasks.run(list);
+        } catch(error) {
+            logger.error(error.message);
+            logger.end();
+        }
     });
 });
