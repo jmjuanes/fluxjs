08879378f357b24c07cd6a92219ab71037390de9
jmjuanes <josemijuanes@gmail.com>
2018-03-09 23:46:13 +0100

    Renamed method flow.option to flow.getOption. Minor code reformat.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let getArgs = require("get-args");
 let logger = require("./lib/logger.js");
 
 //Parse the options
-let options = getArgs().options;
+let taskOptions = getArgs().options;
 
 //Tasks manager
 let tasks = new keue();
@@ -18,12 +18,12 @@ tasks.on("task:start", function (name) {
     taskStart = Date.now();
     return logger.log("Task '" + logger.colors.blue(name) + "' started.");
 });
-tasks.on("task:end", function(name){
+tasks.on("task:end", function (name) {
     taskEnd = Date.now();
     let time = taskEnd - taskStart;
     return logger.log("Task '" + logger.colors.blue(name) + "' finished after " + time + "ms.");
 });
-tasks.on("error", function(error){
+tasks.on("error", function (error) {
     logger.error(error.message);
     logger.end();
 });
@@ -31,11 +31,10 @@ tasks.on("error", function(error){
 //Add a new task
 module.exports.task = function () {
     tasks.addTask.apply(tasks, arguments);
-    //tasksList.push(arguments[0]);
 };
 
 //Set the default task
-module.exports.defaultTask = function(list) {
+module.exports.defaultTask = function (list) {
     if (typeof list === "undefined") {
         tasksList = [];
     }
@@ -54,17 +53,17 @@ module.exports.log = logger.log;
 module.exports.error = logger.error;
 
 //Access to the options passed to the script
-module.exports.option = function(key) {
-    return options[key];
+module.exports.getOption = function (key) {
+    return taskOptions[key];
 };
 
 process.nextTick(function () {
     //Check the no-colors option
-    if(typeof options["flow-no-colors"] === "boolean") {
+    if(typeof taskOptions["flow-no-colors"] === "boolean") {
         logger.disableColors();
     }
     //Get the tasks to run
-    let list = (typeof options["flow-tasks"] === "string") ? options["flow-tasks"].split(",") : tasksList;
+    let list = (typeof taskOptions["flow-tasks"] === "string") ? taskOptions["flow-tasks"].split(",") : tasksList;
     if(list.length === 0) {
         logger.error("No tasks to run");
         return logger.end();
